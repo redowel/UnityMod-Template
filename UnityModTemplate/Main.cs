ec1a2c0639868e88328938de70abe5a265cf65a2
JAKE <75399013+redowel@users.noreply.github.com>
2020-12-08 00:47:44 -0800

    Add files via upload

---
+++ UnityModTemplate/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Collections;
+
+namespace UnityModTemplate
+{
+    public class Main
+    {
+        static void Init()
+        {
+            Main.Load = new GameObject();
+            Main.Load.AddComponent<Mod>();
+            UnityEngine.Object.DontDestroyOnLoad(Main.Load);
+        }
+
+        static void Unload()
+        {
+            GameObject.Destroy(Main.Load);
+        }
+
+
+        private static GameObject Load;
+    }
+}
---
+++ UnityModTemplate/Mod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityModTemplate
+{
+    class Mod : MonoBehaviour
+    {
+        public void OnGUI()
+        {
+            GUI.Box(new Rect(10, 10, 200, 20), "UNITY TEMPLATE BOX!");
+         
+        }
+
+
+
+    }
+}
